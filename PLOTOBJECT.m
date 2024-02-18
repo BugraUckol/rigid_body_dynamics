edaed2497ee126cc61d7877127b85b803e47e734
leadGuitarBG <95097787+BugraUckol@users.noreply.github.com>
2024-02-18 21:41:23 +0300

    [ADD] Coordainate axes are plotted now

--- PLOTOBJECT.m
+++ PLOTOBJECT.m
@@ -15,9 +15,15 @@ BOTTOM = [C1, C2, C3, C4];
 % Create upper locations
 UP = BOTTOM + [0, 0, H]';
 
+% Create center locations
+CENT = zeros(3, 3);
+TIPS = eye(3);
+
 % Convert to homogenous coordinates
 BOTTOMH = [BOTTOM; 1, 1, 1, 1];
 UPH = [UP; 1, 1, 1, 1];
+CENTH = [CENT; 1, 1, 1];
+TIPSH = [TIPS; 1, 1, 1];
 
 % Create the homogenous transformation matrix
 R = [CB2E(E), X; 0, 0, 0, 1];
@@ -25,26 +31,45 @@ R = [CB2E(E), X; 0, 0, 0, 1];
 % Transform edge locations
 BOTTOMHT = R * BOTTOMH;
 UPHT = R * UPH;
+CENTHT = R * CENTH;
+TIPSHT = R * TIPSH;
 
-% Transformed bottom coordinates
+% Transformed coordinates
 BOTTOMT = BOTTOMHT(1:3,:);
 UPT = UPHT(1:3,:);
+CENTT = CENTHT(1:3,:);
+TIPST = TIPSHT(1:3,:);
 
+% Plot
 figure(FIGNO)
 if CLF
     clf
 end
 hold all
 
+% Plot Earth frames
+plot3([0,1],[0,0],[0,0],'r','LineWidth',2);
+plot3([0,0],[0,1],[0,0],'g','LineWidth',2);
+plot3([0,0],[0,0],[0,1],'b','LineWidth',2);
+
+% Plot Body frame
+plot3([CENTT(1,1), TIPST(1,1)], [CENTT(2,1), TIPST(2,1)],...
+    [CENTT(3,1), TIPST(3,1)], 'r','LineWidth',2);
+plot3([CENTT(1,2), TIPST(1,2)], [CENTT(2,2), TIPST(2,2)],...
+    [CENTT(3,2), TIPST(3,2)], 'g','LineWidth',2);
+plot3([CENTT(1,3), TIPST(1,3)], [CENTT(2,3), TIPST(2,3)],...
+    [CENTT(3,3), TIPST(3,3)], 'b','LineWidth',2);
+
+% Plot body
 E1 = [BOTTOMT(:,1),UPT(:,1)]';
 E2 = [BOTTOMT(:,2),UPT(:,2)]';
 E3 = [BOTTOMT(:,3),UPT(:,3)]';
 E4 = [BOTTOMT(:,4),UPT(:,4)]';
 
-plot3(E1(:,1), E1(:,2), E1(:,3),'k');
-plot3(E2(:,1), E2(:,2), E2(:,3),'k');
-plot3(E3(:,1), E3(:,2), E3(:,3),'k');
-plot3(E4(:,1), E4(:,2), E4(:,3),'k');
+plot3(E1(:,1), E1(:,2), E1(:,3),'k','LineWidth',2);
+plot3(E2(:,1), E2(:,2), E2(:,3),'k','LineWidth',2);
+plot3(E3(:,1), E3(:,2), E3(:,3),'k','LineWidth',2);
+plot3(E4(:,1), E4(:,2), E4(:,3),'k','LineWidth',2);
 
 patch(UPT(1,:),UPT(2,:),UPT(3,:),'red')
 patch(BOTTOMT(1,:),BOTTOMT(2,:),BOTTOMT(3,:),'green')
--- motion_test.m
+++ motion_test.m
@@ -4,7 +4,7 @@ clc, clear, close all
 
 % Simulation parameters
 t_s = 0.001;
-t_lim = 100;
+t_lim = 10;
 
 % Rigid body parameters
 m = 1;
@@ -12,7 +12,7 @@ j = eye(3);
 j_i = inv(j);
 
 % Rigid body dimensions
-dims = [1,1,3];
+dims = [0.2 , 0.2, 0.05];
 
 % Allocation
 X = zeros(3,t_lim/t_s);
@@ -21,10 +21,10 @@ V = zeros(3,t_lim/t_s);
 W = zeros(3,t_lim/t_s);
 
 % Initial conditions
-X(:,1) = [0, 0, 0]';
+X(:,1) = [1, 1, 1]';
 E(:,1) = [0, pi/4, 0]';
 V(:,1) = [0, 0, 0]';
-W(:,1) = [0, 0, 0]';
+W(:,1) = [0, 0, 0.5]';
 
 %% Simulation
 step_counter = 0;
@@ -32,7 +32,7 @@ for i = t_s:t_s:t_lim
 
     step_counter = step_counter + 1;
 
-    F = [0, 0, 10]';
+    F = [0, 0, 0.03]';
     M = [0, 0, 0]';
 
     [X_NEW,E_NEW,V_NEW,W_NEW] = RK4UPDATESTATES(m,j,j_i,F,M,...
@@ -44,9 +44,9 @@ for i = t_s:t_s:t_lim
     V(:,step_counter + 1) = V_NEW;
     W(:,step_counter + 1) = W_NEW;
 
-    if mod(step_counter, 100) == 0
-        PLOTOBJECT(3, dims, X_NEW, E_NEW, 1)
-        pause(0.1)
+    if mod(step_counter, 1000) == 0
+        PLOTOBJECT(3, dims, X_NEW, E_NEW, 0)
+        % pause(0.1)
     end
     
 end
