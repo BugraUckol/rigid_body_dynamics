10c5aefd033171285a7c13cae96973835d598e71
leadGuitarBG <95097787+BugraUckol@users.noreply.github.com>
2024-07-29 23:54:36 +0300

    [FASTER]

--- CB2E.m
+++ CB2E.m
@@ -1,5 +1,24 @@
 function C = CB2E(E)
 
-C = CE2B(E)';
+% Transformation between E(2) Earth frame and first intermediate frame
+% Rotating about z-axis for E(3) radians
+CE2F = [ cos(E(3)), sin(E(3)), 0;
+        -sin(E(3)), cos(E(3)), 0;
+                0,        0, 1];
+
+% Transformation between E(2) first and second intermatediate frames
+% Rotating about y-axis for E(2) (E(2)ta) radians
+CF2S = [ cos(E(2)), 0, -sin(E(2));
+                0, 1,         0;
+         sin(E(2)), 0,  cos(E(2))];
+
+% Transformation between E(2) second intermediate frame and body frame
+% Rotating about x-axis for E(1) radians
+CS2B = [ 1,         0,        0;
+         0,  cos(E(1)), sin(E(1));
+         0, -sin(E(1)), cos(E(1))];
+
+% Combination
+C = (CS2B * CF2S * CE2F)';
 
 end
--- CE2B.m
+++ CE2B.m
@@ -1,26 +1,22 @@
 function C = CE2B(E)
 
-PHI = E(1);
-THE = E(2);
-PSI = E(3);
-
-% Transformation between the Earth frame and first intermediate frame
-% Rotating about z-axis for PSI radians
-CE2F = [ cos(PSI), sin(PSI), 0;
-        -sin(PSI), cos(PSI), 0;
+% Transformation between E(2) Earth frame and first intermediate frame
+% Rotating about z-axis for E(3) radians
+CE2F = [ cos(E(3)), sin(E(3)), 0;
+        -sin(E(3)), cos(E(3)), 0;
                 0,        0, 1];
 
-% Transformation between the first and second intermatediate frames
-% Rotating about y-axis for THE (theta) radians
-CF2S = [ cos(THE), 0, -sin(THE);
+% Transformation between E(2) first and second intermatediate frames
+% Rotating about y-axis for E(2) (E(2)ta) radians
+CF2S = [ cos(E(2)), 0, -sin(E(2));
                 0, 1,         0;
-         sin(THE), 0,  cos(THE)];
+         sin(E(2)), 0,  cos(E(2))];
 
-% Transformation between the second intermediate frame and body frame
-% Rotating about x-axis for PHI radians
+% Transformation between E(2) second intermediate frame and body frame
+% Rotating about x-axis for E(1) radians
 CS2B = [ 1,         0,        0;
-         0,  cos(PHI), sin(PHI);
-         0, -sin(PHI), cos(PHI)];
+         0,  cos(E(1)), sin(E(1));
+         0, -sin(E(1)), cos(E(1))];
 
 % Combination
 C = CS2B * CF2S * CE2F;
